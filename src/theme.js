86fa02efffa87fc75faba1f7e37320537dfb1484
Rahat Mahmud <rahat.mahmud@macaulay.cuny.edu>
2020-08-03 20:48:03 -0400

    Update theme.js

--- src/theme.js
+++ src/theme.js
@@ -4,18 +4,12 @@ export default createMuiTheme({
   shape: {
     borderRadius: 0,
   },
-  overrides: {
-    MuiCssBaseline: {
-      '@global': {
-        body: {
-          backgroundColor: '#242424',
-        },
-      },
-    },
-  },
   palette: {
     type: 'dark',
     primary: { light: '#484848', main: '#0a0a0a', dark: '#000000' },
     secondary: { light: '#454545', main: '#1e1e1e', dark: '#000000' },
+    background: {
+      default: '#242424'
+    },
   },
 });
