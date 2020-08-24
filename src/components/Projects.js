8fd66977742805e6df9725cff4e892fcf5d09caf
Rahat Mahmud <rahat.mahmud@macaulay.cuny.edu>
2020-08-23 22:48:19 -0400

    Add screenshots & thumbnails

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -1,7 +1,9 @@
-import React from 'react';
-import { Grid, Button, Card, CardActions, CardContent, Chip, Typography } from '@material-ui/core';
+import React, { useState } from 'react';
+import { Grid, Button, Card, CardActions, CardContent, Collapse, Chip, Typography } from '@material-ui/core';
 import GithubIcon from '@material-ui/icons/GitHub';
+import PhotoOutlinedIcon from '@material-ui/icons/PhotoOutlined';
 import { makeStyles } from '@material-ui/core/styles';
+import Image from "react-image-enlarger";
 import projects from '../projects.json';
 
 const styles = makeStyles((theme) => ({
@@ -17,14 +19,38 @@ const styles = makeStyles((theme) => ({
   }
 }));
 
+function Thumbnail({ src }) {
+  const [zoom, setZoom] = React.useState(false);
+
+  return (
+    <div>
+      <Image
+        zoomed={zoom}
+        src={src}
+        onClick={() => setZoom(true)}
+        onRequestClose={() => setZoom(false)}
+        overlayColor="#0a0a0a"
+      />
+    </div>
+  );
+}
+
 function Projects() {
   const classes = styles();
+  const [expanded, setExpanded] = useState({});
+
+  const handleExpandClick = (id) => {
+    setExpanded({
+      ...expanded,
+      [id]: !expanded[id]
+    });
+  }
 
   return (
     <div className={classes.root}>
       <Grid container spacing={3}>
         {
-          projects.map && projects.map((project) => {
+          projects && projects.map((project, index) => {
             return (
               <Grid item xs={4}>
                 <Card className={classes.card}>
@@ -32,7 +58,7 @@ function Projects() {
                     <Typography variant='h4' gutterBottom>{project.name}</Typography>
                     <Typography variant='body2' gutterBottom>{project.description}</Typography>
                     {
-                      project.skills.map && project.skills.map((skill) => {
+                      project.skills && project.skills.map((skill) => {
                         return (
                           <Chip label={skill} variant="outlined" className={classes.chip}></Chip>
                         )
@@ -41,7 +67,24 @@ function Projects() {
                   </CardContent>
                   <CardActions>
                     <Button size='small' variant="outlined" startIcon={<GithubIcon />} href={project.link}>Github</Button>
+                    <Button size='small' variant="outlined" startIcon={<PhotoOutlinedIcon />} onClick={() => handleExpandClick(index)}>Screenshots</Button>
                   </CardActions>
+                  <Collapse in={expanded[index]} timeout="auto" unmountOnExit>
+                    <Grid
+                      container
+                      direction="row"
+                      justify="space-evenly"
+                      alignItems="center"
+                    >
+                      {
+                        project.images && project.images.map((image, id) => {
+                          return (
+                            <Thumbnail key={id} src={image} />
+                          )
+                        })
+                      }
+                    </Grid>
+                  </Collapse>
                 </Card>
               </Grid>
             )
